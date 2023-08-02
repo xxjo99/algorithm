269aa51d830e307ae054df17b69d8069492f4713
KwangJo Ko <xxjo99@gmail.com>
2023-08-02 16:18:59 +0900

    readme

---
+++ src/stack/programmers_12906.java
@@ -0,0 +1,28 @@
+package stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class programmers_12906 {
+
+    public int[] solution(int[] arr) {
+        List<Integer> answerList = new ArrayList<>();
+        answerList.add(arr[0]);
+
+        int count = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != answerList.get(count)) {
+                answerList.add(arr[i]);
+                count++;
+            }
+        }
+
+        int[] answer = new int[answerList.size()];
+        for (int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
+        }
+
+        return answer;
+    }
+
+}
