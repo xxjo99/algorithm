7118f2f3537390b8c70f0dd5b7ca049fb7fec58f
KwangJo Ko <xxjo99@gmail.com>
2023-08-03 09:13:34 +0900

    stack

---
+++ src/stack/programmers_stack_4.java
@@ -0,0 +1,39 @@
+package stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class programmers_stack_4 {
+
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(new int[] {i, priorities[i]});
+        }
+
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            boolean able = true;
+
+            for (int[] q : queue) {
+                if (q[1] > now[1]) {
+                    able = false;
+                }
+            }
+
+            if (able) {
+                answer++;
+                if (now[0] == location) {
+                    break;
+                }
+            } else {
+                queue.add(now);
+            }
+        }
+
+        return answer;
+    }
+
+}
---
+++ src/stack/programmers_stack_5.java
@@ -0,0 +1,48 @@
+package stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class programmers_stack_5 {
+
+    class Truck {
+        int weight;
+        int time;
+
+        public Truck(int weight, int time) {
+            this.weight = weight;
+            this.time = time;
+        }
+    }
+
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Truck> bridge = new LinkedList<>();
+        int currentWeight = 0;
+        int index = 0 ;
+        int time = 0;
+
+        while (index < truck_weights.length) {
+            time++;
+
+            if (!bridge.isEmpty() && time - bridge.peek().time == bridge_length) {
+                Truck truck = bridge.poll();
+                currentWeight -= truck.weight;
+            }
+
+            if (bridge.size() >= bridge_length) {
+                continue;
+            }
+
+            if (currentWeight + truck_weights[index] > weight) {
+                continue;
+            }
+
+            currentWeight += truck_weights[index];
+            bridge.add(new Truck(truck_weights[index], time));
+
+            index++;
+        }
+
+        return time + bridge_length;
+    }
+}
